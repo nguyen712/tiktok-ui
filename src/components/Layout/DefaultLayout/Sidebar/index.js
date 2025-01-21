f2c3da2f99e2a2bd812981c8b2da12361d33fee7
nguyen712 <buikhoinguyen2001@gmail.com>
2025-01-21 23:30:37 +0700

    Setup DefaultLayout KN

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,8 +1,12 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss"
+
+const cx = classNames.bind(styles)
 function Sidebar() {
     return (
-        <div>
+        <aside className={cx("wrapper")}>
             <h2>Sidebar</h2>
-        </div>
+        </aside>
     );
 }
 
